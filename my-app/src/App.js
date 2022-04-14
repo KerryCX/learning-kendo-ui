ef6dec4e5842e8076370e526f547ef833a36db90
Kerry Clements <kerryclements@me.com>
2022-04-14 23:16:22 +0100

    set up

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        Surfs Up
+      </header>
+      <body>
+        <main>
+            Where do you want to surf, lets find a time
+        </main>
+      </body>
+      <footer>Thank you</footer>
+    </div>
+  );
+}
+
+export default App;
